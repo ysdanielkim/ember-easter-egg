d67737d30f533fee7578f418b4446459d0e9d5de
Jeroen Schaftenaar <jeroen@amsdfw.us>
2014-12-16 16:57:35 -0600

    Refactoring, peanut support and added screenshot

--- app/components/easter-egg.js
+++ app/components/easter-egg.js
@@ -3,7 +3,6 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 
     easterEggStyle: function() {
-        
         return 'width: '+this.blocksH*this.blockSize+'px; height:'+this.blocksV*this.blockSize+'px;';
     }.property(),
 
@@ -11,17 +10,33 @@ export default Ember.Component.extend({
     blocksV: 29,
     blockSize: 20,
     direction: 'left',
+    oldDirection: 'left',
     mainLoop: null, // attach the interval to this, we will have a way to stop it
     loopDelay: 1000/10,
-    collision: false,
+
+    // make sure that the snake cannot go against itself
+    directionCheck: function() {
+        var gs = this,
+            check = [
+            ['up','down'],
+            ['left','right']
+        ];
+        check.forEach (function(item) {
+            if ( (gs.oldDirection === item[0] && gs.direction === item[1]) ||
+                 (gs.oldDirection === item[1] && gs.direction === item[0])) {
+                    gs.direction = gs.oldDirection;
+                 }
+        });
+    },
 
     computeSnake: function() {
-        var level = this.get('level'),
-            newPosition,
-            snake;
-        snake = level.snake;
+        var newPosition,
+            snake = this.get('snake'),
+            peanut = this.get('peanut');
+
         snake.reverseObjects();
         newPosition = snake[snake.length-1];
+        this.directionCheck();
         switch (this.direction) {
             case 'up':
                 newPosition -= this.blocksH;
@@ -39,77 +54,124 @@ export default Ember.Component.extend({
                 newPosition--;
             break;
         }
+        this.oldDirection = this.direction;
         snake.pushObject(newPosition);
         snake.reverseObjects();
-        snake.popObject();
-        level.snake = snake;
+        if (snake[0]!==peanut) {
+            snake.popObject();
+        } else {
+            this.computePeanut();
+        }
     },
 
     computeLevel: function() {
-        var level = this.get('level');
-        level.blocks.forEach(function(item, index) {
-            if (level.snake.contains(index)) {
+        var blocks = this.get('blocks'),
+            snake = this.get('snake'),
+            peanut = this.get('peanut');
+        blocks.forEach(function(item, index) {
+            if (peanut === index) {
+                Ember.set(item, 'isFloor', false);
+                Ember.set(item, 'isSnake', false);
+                Ember.set(item, 'isPeanut', true);
+            } else if (snake.contains(index)) {
                 Ember.set(item, 'isFloor', false);
                 Ember.set(item, 'isSnake', true);
+                Ember.set(item, 'isPeanut', false);
             } else {
                 Ember.set(item, 'isFloor', true);
                 Ember.set(item, 'isSnake', false);
+                Ember.set(item, 'isPeanut', false);
             }
         });
     },
 
-    collisionCheck: function() {
+    computePeanut: function() {
+        var snake = this.get('snake'),
+            blocks = this.get('blocks'),
+            i = false;
 
+        while (i===false) {
+            i = Math.floor((Math.random() * blocks.length) + 0);
+            if (snake.contains(i)) {
+                i = false;
+            }
+        }
+        this.set('peanut', i);
+    },
+
+    collisionOccured: function() {
+        var blocks = this.get('blocks'),
+            snake = this.get('snake'),   
+            i = 1,
+            collision = false;
+
+        // check if collided with self
+        for (; i<snake.length; i++) {
+            if (snake[0] === snake[i]) {
+                collision = true;
+                break;
+            }
+        }
+
+        // check if snake has collided, top or bottom
+        if (snake[0]<0) {
+            collision = true;
+        }
+        if (snake[0] >= blocks.length) {
+            collision = true;
+        }
+
+        // collision check left side
+        if (((snake[0]+1) % (this.blocksH)) === 0 &&
+            (snake[1] % (this.blocksH)) === 0)  {
+            collision = true;
+        }
+
+        // collision check right side
+        if (((snake[0]) % (this.blocksH)) === 0 &&
+            ((snake[1]+1) % (this.blocksH)) === 0) {
+            collision = true;
+         }
+
+        return collision;
     },
 
     gameLoop: function(gs) {
         gs.computeSnake();
-        gs.computeLevel();
+        if (gs.collisionOccured()) {
+            console.log('collision happened, bad driver!');
+            window.clearInterval(gs.mainLoop);
+        } else {
+            gs.computeLevel();
+        }
     },
 
     initialize: function() {
         var gs = this,
-            level = Ember.Object.create({
-                snake: Ember.A(),
-                peanuts: Ember.A(),
-                blocks: Ember.A()
-            }),
             length = 6,
+            blocks = Ember.A(),
+            snake = Ember.A(),
             i = 0;
 
-        // initial level setup
-        for (i=0;i<(this.blocksH * this.blocksV);i++) {
-            level.blocks.pushObject({ isFloor: true, isSnake: false, isPeanut: false });
+        for (i;i<(this.blocksH * this.blocksV);i++) {
+            blocks.pushObject({ isFloor: true, isSnake: false, isPeanut: false, index: i });
         }
         for (i=0;i<length;i++) {
-            level.snake.pushObject(((level.blocks.length/2)-(length/2))+i);
+            snake.pushObject(((blocks.length/2)-(length/2))+i);
         }
-        this.set('level', level);
+        this.set('snake', snake);
+        this.set('blocks', blocks);
+        this.set('peanut', Ember.A());
+        this.computePeanut();
         this.computeLevel();        
 
-        // keyboard controls
         window.addEventListener('keydown', function (e) {
             var keyTrans = {37: 'left', 38: 'up', 39: 'right', 40:'down'};
-            switch (gs.direction) {
-                case 'left':
-                    delete keyTrans[39];
-                break;
-                case 'right':
-                    delete keyTrans[37];
-                break;
-                case 'up':
-                    delete keyTrans[40];
-                break;
-                case 'down':
-                    delete keyTrans[38];
-                break;
-            }
             if (keyTrans.hasOwnProperty(e.keyCode)) {
                 gs.direction = keyTrans[e.keyCode];
             }
         }, false);
 
-        // mainloop!
         this.mainLoop = setInterval(this.gameLoop, this.loopDelay, gs);
 
     }.on( 'init' )
